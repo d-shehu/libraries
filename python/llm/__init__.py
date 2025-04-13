9c9f25d3b7bbd613407aedd709563304af4799d8
automaton <automaton@datascape.org>
2025-04-12 20:03:23 -0400

    Fix llm package init

--- python/llm/__init__.py
+++ python/llm/__init__.py
@@ -2,11 +2,8 @@ import os
 import subprocess
 import sys
 
-def init():
-    packagePath = os.path.abspath(os.path.dirname(__file__))
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "-qr", 
-                           os.path.join(packagePath, "requirements.txt")])
+packagePath = os.path.abspath(os.path.dirname(__file__))
+subprocess.check_call([sys.executable, "-m", "pip", "install", "-qr", 
+                        os.path.join(packagePath, "requirements.txt")])
 
-    from .src import llm_manager
-
-init()
+from .src import llm_manager
